4a313dc6948a7036dc9dbc82d020c3119c3a4060
Ann-Annice <annannice98@gmail.com>
2022-10-10 09:27:17 +0530

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OnlineshoppingComponent } from './onlineshopping/onlineshopping.component';
 import { MenComponent } from './men/men.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OnlineshoppingComponent,
-    MenComponent
+    MenComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
